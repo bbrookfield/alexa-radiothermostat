48fbc962fa5ff5136fdf711ce0946b027dcced85
tball78 <tball78@gmail.com>
2016-12-01 13:19:42 -0500

    Update index.js to include fan on/off, hold on/off, increase/decrease temp functionality
    Update index.js to include fan on/off, hold on/off, increase/decrease temp functionality
    
    Also:
    
    1) hard coded local IP for thermostat versus using process.env, 2) added logic to check which mode thermostat is in so that code can modify t_heat or t_cool when asking Alexa to raise/lower temperature, 3) edited Alexa response to request for current temperature so that responds with actual temp, current mode (heat, cool, off), target temp.
    
    Associated Intents Schema:
    {
      "intents": [
        {
        "intent": "getTemp",
          "slots": [{
            "name": "getTemperature",
            "type": "AMAZON.LITERAL"
          }]
        },
        {
        "intent": "setHeat",
          "slots": [{
            "name": "setHeating",
            "type": "AMAZON.NUMBER"
          }]
        },
        {
        "intent": "setCool",
          "slots": [{
            "name": "setCooling",
            "type": "AMAZON.NUMBER"
          }]
        },
        {
        "intent": "setOff"
        },
        {
        "intent": "setFanOn"
        },
        {
        "intent": "setFanAuto"
        },
        {
        "intent": "setHoldOn"
        },
        {
        "intent": "setHoldOff"
        },
        {
        "intent": "incTemp",
          "slots": [{
            "name": "increaseTemp",
            "type": "AMAZON.NUMBER"
          }]
        },
        {
        "intent": "decTemp",
          "slots": [{
            "name": "decreaseTemp",
            "type": "AMAZON.NUMBER"
          }]
        }
      ]
    }
        
    Revised sample utterances:
    
    setOff to turn off thermostat
    setOff to turn thermostat off
    setOff set thermostat to off
    setFanOn to turn on fan
    setFanOn to turn on blower
    setFanOn to turn blower on
    setFanOn to turn fan on
    setFanAuto to turn off fan
    setFanAuto to turn off blower
    setFanAuto to turn blower off
    setFanAuto to turn fan off
    setFanOn to set fan to on
    setFanOn to set blower to on
    setFanAuto to set fan to off
    setFanAuto to set fan to auto
    setFanAuto to set blower to auto
    setHoldOn to turn on hold
    setHoldOn to turn hold on
    setHoldOn to set hold to on
    setHoldOff to turn off hold
    setHoldOff to turn hold off
    setHoldOff to set hold to off
    setHeat to set heat to {setHeating}
    setCool to adjust cool to {setCooling}
    setHeat to adjust heat to {setHeating}
    setCool to set cool to {setCooling}
    setCool to adjust air conditioning to {setCooling}
    setCool to set air conditioning to {setCooling}
    setCool to set air to {setCooling}
    setCool to adjust air to {setCooling}
    setHeat to adjust the heat to {setHeating}
    setCool to adjust the air conditioning to {setCooling}
    setCool to set ac to {setCooling}
    setCool to adjust ac to {setCooling}
    setCool to set air conditioning to {setCooling}
    getTemp what is the temperature
    getTemp what is temperature
    getTemp what's the temperature
    getTemp say the temperature
    getTemp tell me the temperature
    getTemp tell me temperature
    incTemp raise target temperature by {increaseTemp}
    incTemp increase target temperature by {increaseTemp}
    decTemp lower target temperature by {decreaseTemp}
    decTemp decrease target temperature by {decreaseTemp}

--- index.js
+++ index.js
@@ -1,32 +1,140 @@
 'use strict';
 var request = require('request');
 var alexa = require('alexa-app');
-
+var newtemp 
+var oldtemp
 var thermostatApp = new alexa.app('thermostat');
 
-thermostatApp.intent('setTemp', function(req, res) {
-  request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_cool: parseFloat(req.slot('setTemperature'))}});
-  res.card("Thermostat Skill","Thermostat is set to " + parseInt(req.slot('setTemperature')) + " degrees");
-  res.say("Thermostat is set to " + parseInt(req.slot('setTemperature')) + " degrees");
+thermostatApp.intent('setHeat', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {t_heat: parseFloat(req.slot('setHeating'))}});
+  res.card("Thermostat Skill","Thermostat heat mode is set to " + parseInt(req.slot('setHeating')) + " degrees");
+  res.say("Thermostat heat mode is set to " + parseInt(req.slot('setHeating')) + " degrees");
+});
+
+thermostatApp.intent('setCool', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {t_cool: parseFloat(req.slot('setCooling'))}});
+  res.card("Thermostat Skill","Thermostat cool mode is set to " + parseInt(req.slot('setCooling')) + " degrees");
+  res.say("Thermostat cool mode is set to " + parseInt(req.slot('setCooling')) + " degrees");
+});
+
+thermostatApp.intent('setOff', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {"tmode": 0}});
+  res.card("Thermostat Skill","Thermostat is set to off");
+  res.say("Thermostat is set to off");
+});
+
+thermostatApp.intent('setFanOn', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {"fmode": 2}});
+  res.card("Thermostat Skill","Thermostat fan is set to on");
+  res.say("Thermostat fan is set to on");
+});
+
+thermostatApp.intent('setFanAuto', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {"fmode": 0}});
+  res.card("Thermostat Skill","Thermostat fan is set to auto");
+  res.say("Thermostat fan is set to auto");
+});
+
+thermostatApp.intent('setHoldOn', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {"hold": 1}});
+  res.card("Thermostat Skill","Thermostat hold is set to on");
+  res.say("Thermostat hold is set to on");
+});
+
+thermostatApp.intent('setHoldOff', function(req, res) {
+  request.post('http://192.168.1.208/tstat', {json: {"hold": 0}});
+  res.card("Thermostat Skill","Thermostat hold is set to off");
+  res.say("Thermostat hold is set to off");
 });
 
 // process get temperature request
 // This intent uses the res.send() feature for a delayed response back to alexa due to async http request.
 thermostatApp.intent('getTemp', function(req, res) {
-  request(process.env.THERMOSTAT_URL + '/tstat', function (error, response, body) {
+  request('http://192.168.1.208/tstat', function (error, response, body) {
     console.log('Error: ' + error, 'RESPONSE: ' + response, 'BODY: ' + body);
     body = JSON.parse(body);
-    res.say("Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
-    res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
-    res.send();
+    if (body.tmode == 0) {
+        res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.send();
+    }
+    if (body.tmode == 1) {
+        res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is in heat mode. Target temperature is " + body.t_heat + " degrees.");
+        res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is in heat mode. Target temperature is " + body.t_heat + " degrees.");
+        res.send();
+    }       
+    if (body.tmode == 2) {
+        res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is in cool mode. Target temperature is " + body.t_cool + " degrees.");
+        res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is in cool mode. Target temperature is " + body.t_cool + " degrees.");
+        res.send();
+    }
   });
   return false;
 });
 
+// process increase temperature request
+// This intent uses the res.send() feature for a delayed response back to alexa due to async http request.
+thermostatApp.intent('incTemp', function(req, res) {
+  request('http://192.168.1.208/tstat', function (error, response, body) {
+    console.log('Error: ' + error, 'RESPONSE: ' + response, 'BODY: ' + body);
+    body = JSON.parse(body);
+    if (body.tmode == 0) {
+        res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.send();
+    }
+    if (body.tmode == 1) {
+        newtemp = body.t_heat + parseInt(req.slot('increaseTemp'));
+        request.post('http://192.168.1.208/tstat', {json: {t_heat: parseInt(newtemp)}});
+        res.say("Thermostat is in heat mode. Target temperature increased by " + parseInt(req.slot('increaseTemp')) + " degrees.  Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.card("Thermostat Skill","Thermostat is in heat mode. Target temperature increased by " + parseInt(req.slot('increaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.send();
+    }
+    if (body.tmode == 2) {
+        newtemp = body.t_cool + parseInt(req.slot('increaseTemp'));
+        request.post('http://192.168.1.208/tstat', {json: {t_cool: parseInt(newtemp)}});
+        res.say("Thermostat is in cool mode. Target temperature increased by " + parseInt(req.slot('increaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.card("Thermostat Skill","Thermostat is in cool mode. Target temperature increased by " + parseInt(req.slot('increaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.send();
+    }
+  });
+  return false;
+});
+
+// process decrease temperature request
+// This intent uses the res.send() feature for a delayed response back to alexa due to async http request.
+thermostatApp.intent('decTemp', function(req, res) {
+  request('http://192.168.1.208/tstat', function (error, response, body) {
+    console.log('Error: ' + error, 'RESPONSE: ' + response, 'BODY: ' + body);
+    body = JSON.parse(body);
+    if (body.tmode == 0) {
+        res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is off.");
+        res.send();
+    }
+    if (body.tmode == 1) {
+        newtemp = body.t_heat - parseInt(req.slot('decreaseTemp'));
+        request.post('http://192.168.1.208/tstat', {json: {t_heat: parseInt(newtemp)}});
+        res.say("Thermostat is in heat mode. Target temperature decreased by " + parseInt(req.slot('decreaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.card("Thermostat Skill","Thermostat is in heat mode. Target temperature decreased by " + parseInt(req.slot('decreaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.send();
+    }
+    if (body.tmode == 2) {
+        newtemp = body.t_cool - parseInt(req.slot('decreaseTemp'));
+        request.post('http://192.168.1.208/tstat', {json: {t_cool: parseInt(newtemp)}});
+        res.say("Thermostat is in cool mode. Target temperature decreased by " + parseInt(req.slot('decreaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.card("Thermostat Skill","Thermostat is in cool mode. Target temperature decreased by " + parseInt(req.slot('decreaseTemp')) + " degrees. Target temperature is now " + parseInt(newtemp) + " degrees.");
+        res.send();
+    }
+  });
+  return false;
+});
+
+
 // process launch request when no utterances detected
 thermostatApp.launch(function(req, res) {
   console.log('REQUEST', JSON.stringify(req));
-  res.say("You can say, what is the temperature, or set the temperature to 75!");
+  res.say("You can say, what is the temperature, set heat to 75, or set cool to 72, turn hold on, turn hold off, turn fan on, turn fan off.");
 });
 
 module.exports = function(cb) {
